afd9fbfac26ff850b2b105e72db7301645a184b4
vishwa-karthik <vishwa.prarthana@gmail.com>
2022-04-23 15:06:58 +0530

    final

--- 10_Abstract_Classes.dart
+++ 10_Abstract_Classes.dart
@@ -1,3 +1,24 @@
 void main() {
-  print('Hello world');
+  Lion animal1 = Lion();
+  animal1.helloAnimals();
+
+  Lion animal2 = Lion();
+  animal2.NotHuman();
+}
+
+abstract class Animals {
+  void helloAnimals() {}
+  void NotHuman() {}
+}
+
+class Lion extends Animals {
+  @override
+  void helloAnimals() {
+    print('Hello Lion');
+  }
+
+  @override
+  void NotHuman() {
+    print('Not human also');
+  }
 }
